3b674d62e867373cb869498f467e3798ef7916ed
Bashir Uddin <uddinbashir1999@gmail.com>
2023-04-20 14:17:29 +0500

    fix: add icons

---
+++ src/icons/ethereum-icon.js
@@ -0,0 +1,20 @@
+export const SvgEthereumIcon = ({ width = "24", height = "37" }) => {
+  return (
+    <svg
+      width={width}
+      height={height}
+      viewBox="0 0 24 37"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M0 18.3768L11.6572 0.376221L24 18.3768L11.6572 24.7895L0 18.3768Z"
+        fill="#201D1D"
+      />
+      <path
+        d="M24 20.9348L11.6572 27.3993L0 20.9348L11.6572 36.3537L24 20.9348Z"
+        fill="#201D1D"
+      />
+    </svg>
+  );
+};
---
+++ src/icons/index.js
@@ -0,0 +1,4 @@
+export * from "./ethereum-icon";
+export * from "./medium-icon";
+export * from "./telegram-icon";
+export * from "./twitter-icon";
